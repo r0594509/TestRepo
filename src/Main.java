c5d45aa900c343b05b256b4fe31f2ade18cb404a
Nathan O <r0594509@users.noreply.github.com>
2017-12-16 17:29:46 +0100

    Simulating merge

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("This is a second commit.");
+		System.out.println("This is a third, remotely edited, commit.");
 		
 	}
 	
